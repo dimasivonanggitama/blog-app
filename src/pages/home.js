ac82f7ca9caefe515e6670bfc1cfa6ef48deb648
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-06-26 15:17:38 +0700

    v2.2 Relayouting home page: integrating with Header.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../components/navbar'
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import Header from '../components/Header'
+import { Box, Grid, GridItem, Text } from '@chakra-ui/react'
 import Content from '../components/Content'
 
 
@@ -11,26 +11,20 @@ const HomePage = () => {
     //   <Content/>
     // </Box>
     <Grid
-      // templateAreas={`"header header"
-                      // "nav main"
-                      // "nav footer"`}
-      // gridTemplateRows={'50px 1fr 30px'}
-      // gridTemplateColumns={'150px 1fr'}
-
       templateAreas={`"header header header"
                       "leftMargin carousel rightMargin"
                       "leftMargin navbar rightMargin"
                       "leftMargin main rightMargin"
                       "footer footer footer"`}
-      gridTemplateRows={'50px 1fr 1fr 1fr 30px'}
+      gridTemplateRows={'0fr 1fr 1fr 1fr 30px'}
       gridTemplateColumns={'5% 1fr 5%'}
       h='500px'
       gap='1'
       color='blackAlpha.700'
       fontWeight='bold'
     >
-      <GridItem pl='2' bg='orange.300' area={'header'}>
-        Header
+      <GridItem bg='orange.300' area={'header'}>
+        <Header/>
       </GridItem>
       <GridItem pl='2' bg='pink.300' area={'leftMargin'}>
        Left Margin
