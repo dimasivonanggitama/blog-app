0176ed61067a15a9f8bb5d3467b01908d7eb9aa6
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-06-26 16:01:58 +0700

    v2.2.2 Added Search Bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import { SearchIcon } from '@chakra-ui/icons'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <>
+        <InputGroup>
+            <InputLeftElement pointerEvents='none'>
+                <SearchIcon color='gray.300' />
+            </InputLeftElement>
+            <Input bg={"white"} type='text' placeholder='Cari sesuatu ...'/>
+        </InputGroup>
+    </>
+  )
+}
+
+export default SearchBar
