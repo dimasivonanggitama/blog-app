3c90e003a2874ff08755bd0c7bf61b789f461380
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-06-26 15:00:22 +0700

    v2.1 relayouting home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,16 +12,19 @@ const HomePage = () => {
     // </Box>
     <Grid
       // templateAreas={`"header header"
-      //                 "nav main"
-      //                 "nav footer"`}
+                      // "nav main"
+                      // "nav footer"`}
       // gridTemplateRows={'50px 1fr 30px'}
       // gridTemplateColumns={'150px 1fr'}
-      templateAreas={`"header"
-                      "main"
-                      "footer"`}
-      gridTemplateRows={'50px 50px'}
-      gridTemplateColumns={'150px 1fr'}
-      h='200px'
+
+      templateAreas={`"header header header"
+                      "leftMargin carousel rightMargin"
+                      "leftMargin navbar rightMargin"
+                      "leftMargin main rightMargin"
+                      "footer footer footer"`}
+      gridTemplateRows={'50px 1fr 1fr 1fr 30px'}
+      gridTemplateColumns={'5% 1fr 5%'}
+      h='500px'
       gap='1'
       color='blackAlpha.700'
       fontWeight='bold'
@@ -29,11 +32,20 @@ const HomePage = () => {
       <GridItem pl='2' bg='orange.300' area={'header'}>
         Header
       </GridItem>
-      {/* <GridItem pl='2' bg='pink.300' area={'nav'}>
-        Nav
-      </GridItem> */}
+      <GridItem pl='2' bg='pink.300' area={'leftMargin'}>
+       Left Margin
+      </GridItem>
+      <GridItem pl='2' bg='purple.300' area={'carousel'}>
+        Carousel
+      </GridItem>
+      <GridItem pl='2' bg='yellow.300' area={'navbar'}>
+        Navbar
+      </GridItem>
       <GridItem pl='2' bg='green.300' area={'main'}>
         Main
+      </GridItem>
+      <GridItem pl='2' bg='pink.300' area={'rightMargin'}>
+       Right Margin
       </GridItem>
       <GridItem pl='2' bg='blue.300' area={'footer'}>
         Footer
