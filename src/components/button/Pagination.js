44b4db5d3b5e645813938866d6c0c4d42aca7bb3
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-09 18:33:26 +0700

    v1.4.3.2 Added pagination components

--- src/components/button/Pagination.js
+++ src/components/button/Pagination.js
@@ -1,8 +1,19 @@
+import { Button, Center, Container, HStack, IconButton, Input, Text } from '@chakra-ui/react'
 import React from 'react'
+import { PiArrowLeft, PiArrowRight, PiArrowLineLeftBold, PiArrowLineRightBold } from 'react-icons/pi'
 
 const Pagination = () => {
   return (
-    <div>Pagination</div>
+    // <HStack>
+        <Center gap="2">
+        <IconButton aria-label='Left end' icon={<PiArrowLineLeftBold/>} bgColor='transparent'/>
+        <IconButton aria-label='Next left' icon={<PiArrowLeft/>} bgColor='transparent'/>
+        <Input value="135" maxWidth="50"/>
+        <Text>dari 150</Text>
+        <IconButton aria-label='Next right' icon={<PiArrowRight/>} bgColor='transparent'/>
+        <IconButton aria-label='Right end' icon={<PiArrowLineRightBold/>} bgColor='transparent'/>
+        </Center>
+    // </HStack>
   )
 }
 
